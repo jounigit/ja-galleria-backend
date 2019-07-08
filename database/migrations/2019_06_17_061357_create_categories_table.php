ea824bfd59a32fde612ea51ff79205745ace0458
Jouni <airaksinen.jouni@gmail.com>
2019-07-08 11:48:44 +0300

    Album resource and collection

--- database/migrations/2019_06_17_061357_create_categories_table.php
+++ database/migrations/2019_06_17_061357_create_categories_table.php
@@ -15,7 +15,7 @@ class CreateCategoriesTable extends Migration
     {
         Schema::create('categories', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->unsignedBigInteger('user_id');
+            $table->unsignedBigInteger('user_id')->nullable();
             $table->string('title');
             $table->string('slug');
             $table->text('content')->nullable();
