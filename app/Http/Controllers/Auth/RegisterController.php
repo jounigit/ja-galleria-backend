1d4373f64911c0a05bb70f84d0da86aae3fcf9ae
Jouni <airaksinen.jouni@gmail.com>
2019-07-10 17:45:38 +0300

    Login- and Registercontrollertest

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -25,38 +25,6 @@ class RegisterController extends Controller
     use RegistersUsers;
 
     /**
-     * Where to redirect users after registration.
-     *
-     * @var string
-     */
-    // protected $redirectTo = '/home';
-
-    /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
-    // public function __construct()
-    // {
-    //     $this->middleware('guest');
-    // }
-
-    /**
-     * Get a validator for an incoming registration request.
-     *
-     * @param  array  $data
-     * @return \Illuminate\Contracts\Validation\Validator
-     */
-    // protected function validator(array $data)
-    // {
-    //     return Validator::make($data, [
-    //         'name' => ['required', 'string', 'max:255'],
-    //         'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
-    //         'password' => ['required', 'string', 'min:8', 'confirmed'],
-    //     ]);
-    // }
-
-    /**
      * Create a new user instance after a valid registration.
      *
      * @param  \Illuminate\Http\Request  $request
--- tests/Feature/Auth/LoginControllerTest.php
+++ tests/Feature/Auth/LoginControllerTest.php
@@ -4,7 +4,6 @@ namespace Tests\Feature\Auth;
 
 use Tests\TestCase;
 use App\User;
-use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Artisan;
 
@@ -26,8 +25,12 @@ class LoginControllerTest extends TestCase
         ]);
     }
 
-
-    public function testRequireEmailAndLogin()
+    /**
+     * Test email and password are required.
+     *
+     * @return void
+     */
+    public function testRequireEmailAndPassword()
     {
         $this->json('POST', 'api/login')
             ->assertStatus(422)
@@ -38,9 +41,85 @@ class LoginControllerTest extends TestCase
                     'password' => ['The password field is required.']
                 ]
             ]);
-
     }
 
+    /**
+     * Test email is required.
+     *
+     * @return void
+     */
+    public function testRequireEmail()
+    {
+        $user = ['password' => 'userpass'];
+        $this->json('POST', 'api/login', $user)
+            ->assertStatus(422)
+            ->assertJson([
+                'message' => 'The given data was invalid.',
+                'errors' => [
+                    'email' => ['The email field is required.']
+                ]
+            ]);
+    }
+
+        /**
+     * Test password is required.
+     *
+     * @return void
+     */
+    public function testRequirePassword()
+    {
+        $user = ['email' => 'user@email.com'];
+        $this->json('POST', 'api/login', $user)
+            ->assertStatus(422)
+            ->assertJson([
+                'message' => 'The given data was invalid.',
+                'errors' => [
+                    'password' => ['The password field is required.']
+                ]
+            ]);
+    }
+
+          /**
+     * Test with wrong password.
+     *
+     * @return void
+     */
+    public function testWrongPassword()
+    {
+        $user = ['email' => 'user@email.com', 'password' => 'wrongpass'];
+        $this->json('POST', 'api/login', $user)
+            ->assertStatus(422)
+            ->assertJson([
+                'message' => 'The given data was invalid.',
+                'errors' => [
+                    'email' => ['These credentials do not match our records.']
+                ]
+            ]);
+    }
+
+         /**
+     * Test with wrong email.
+     *
+     * @return void
+     */
+    public function testWrongEmail()
+    {
+        $user = ['email' => 'wrong@email.com', 'password' => 'userpass'];
+        $this->json('POST', 'api/login', $user)
+            ->assertStatus(422)
+            ->assertJson([
+                'message' => 'The given data was invalid.',
+                'errors' => [
+                    'email' => ['These credentials do not match our records.']
+                ]
+            ]);
+    }
+
+    /**
+     * Test login successfully.
+     *
+     * @return void
+     */
     public function testUserLoginSuccessfully()
     {
         $user = ['email' => 'user@email.com', 'password' => 'userpass'];
