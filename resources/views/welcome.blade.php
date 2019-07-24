8746c6e57bc83a54bdc016856a05e4f84d2ccd03
Jouni <airaksinen.jouni@gmail.com>
2019-07-24 07:00:25 +0300

    master

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                     <?php echo public_path(7); ?><br>
-                    <?php echo Storage::directories(directory); ?><br>
+                    
                     <img src="{{ url('7/images/image-1563197479.jpg') }}">
                 <div class="title m-b-md">
                     Laravel
